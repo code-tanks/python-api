2d593afa1af696945970c58bc70c544597082965
Derrick Liu <me@derrickliu.dev>
2022-09-04 23:57:11 +0000

    changes

--- codetanks/__init__.py
+++ codetanks/__init__.py
@@ -1,2 +1,15 @@
-def joke():
-    return "hello"
+import codetanks.commands
+from abc import ABC, abstractmethod
+
+
+class BaseTank(ABC):
+    def __init__(self):
+        self.commands = []
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    @abstractmethod
+    def on_event(self):
+        pass
---
+++ tanks/my_tank.py
@@ -0,0 +1,11 @@
+from codetanks import *
+
+
+class MyTank(BaseTank):
+    def run(self):
+        # TODO
+        pass
+
+    def on_event(self):
+        # TODO
+        pass
